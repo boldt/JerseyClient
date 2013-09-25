c75f530de4758b16fd49d3fae422d4c23c407c3a
Dennis Boldt <boldt@itm.uni-luebeck.de>
2013-09-25 16:46:07 +0200

    Use MediaType instead of plain text.

--- src/main/java/de/dennis_boldt/JerseyClient.java
+++ src/main/java/de/dennis_boldt/JerseyClient.java
@@ -1,5 +1,7 @@
 package de.dennis_boldt;
 
+import javax.ws.rs.core.MediaType;
+
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
 
@@ -8,10 +10,10 @@ public class JerseyClient {
 	public static void main(String[] args) {
 		Client client = Client.create();
 		WebResource webResource = client.resource("http://localhost:11111/helloworld");
-		String textPlain = webResource.accept("text/plain").get(String.class);
+		String textPlain = webResource.accept(MediaType.TEXT_PLAIN).get(String.class);
 		System.out.println(textPlain);
 
-		String textHtml = webResource.accept("text/html").get(String.class);
+		String textHtml = webResource.accept(MediaType.TEXT_HTML).get(String.class);
 		System.out.println(textHtml);
 	}
 }
