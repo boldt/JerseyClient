d9795abc3e6ed338d7f41a95b83b9008b6e2d4cb
Dennis Boldt <boldt@itm.uni-luebeck.de>
2013-09-25 16:46:56 +0200

    Comments

--- src/main/java/de/dennis_boldt/JerseyClient.java
+++ src/main/java/de/dennis_boldt/JerseyClient.java
@@ -10,9 +10,12 @@ public class JerseyClient {
 	public static void main(String[] args) {
 		Client client = Client.create();
 		WebResource webResource = client.resource("http://localhost:11111/helloworld");
+
+		// GET as plain text
 		String textPlain = webResource.accept(MediaType.TEXT_PLAIN).get(String.class);
 		System.out.println(textPlain);
 
+		// GET as html
 		String textHtml = webResource.accept(MediaType.TEXT_HTML).get(String.class);
 		System.out.println(textHtml);
 	}
